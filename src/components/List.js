ea45e9d5391410ef24abb06c52c66c6686e5a1b9
Precious <Preshomondi@gmail.com>
2024-02-02 02:54:26 +0300

    positioning

--- src/components/List.js
+++ src/components/List.js
@@ -1,27 +1,35 @@
-import React, {useEffect, useState} from 'react'
-import '../components/List.css'
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import "../components/List.css";
 
 function List() {
-    const [foodsData, setFoodsData] = useState([])
+  const [foodsData, setFoodsData] = useState([]);
 
-    useEffect(() => {
-        fetch('https://presho99.github.io/skfoods/foods')
-        .then(response => response.json())
-        .then(data => setFoodsData(data.foodsData))
-        .catch(error => console.error('Error fetching data:', error))
-    }, [])
+  useEffect(() => {
+    fetch("https://presho99.github.io/skfoods/foods")
+      .then((response) => response.json())
+      .then((data) => setFoodsData(data.foodsData))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
   return (
-    <div className='list'>
-        <ul>
-            {foodsData.map((food) => (
-                <li key={food.id}>
-                    <img src={food.avatar} className="image"/>
-                    <p>{food.name}</p>
-                </li>
-            ))}
-        </ul>
+    <div className="list">
+      <div  className="list-container">
+      <FontAwesomeIcon icon={faAngleLeft} className="arrow"/>
+        <div className="itself-box">
+         
+          {foodsData.map((food) => (
+            <div key={food.id} className="food-itself">
+              <img src={food.avatar} className="image" />
+              <p className="name">{food.name}</p>
+            </div>
+          ))}
+         
+        </div>
+        <FontAwesomeIcon icon={faAngleRight} className="arrow"/>
+      </div>
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
