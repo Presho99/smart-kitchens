b4334d5ff25f8c4e5eaefe8102e5551d03338a1e
Precious <Preshomondi@gmail.com>
2024-02-02 23:46:49 +0300

    Randomize background color

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,14 +1,33 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { faMagnifyingGlass, faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import '../components/Review.css'
 
+const getRandomColor = () => {
+    const colors = ['#F5CE64', '#FAAA67', '#F7B0BC', '#BBCBA1']
+    const randomIndex = Math.floor(Math.random() * colors.length)
+    return colors[randomIndex]
+}
+
 function Review() {
     const [activeTab, setActiveTab] = useState('Overview')
+    const [currentColor, setCurrentColor] = useState(getRandomColor())
+    
+
+  
+
+    useEffect(() => {
+        const colorChangeInterval = setInterval(() => {
+            setCurrentColor(getRandomColor())
+        }, 3000)
+        return () => clearInterval(colorChangeInterval)
+    }, [])
 
     const handleTabClick = (tab) => {
         setActiveTab(tab)
     }
+
+  
   return (
     <div className='review'>
     <div className='review-nav'>
@@ -19,6 +38,11 @@ function Review() {
         <div className='box-nav'>
             <p className={activeTab === 'Overview' ? 'active' : ''} onClick={() => handleTabClick('Overview')}>Overview </p>
             <p className={activeTab ==='Ingredients' ? 'active' : ''} onClick={() => handleTabClick('Ingredients')}>Ingredients </p>
+        </div>
+        <div className='rating'>
+            <div className='rating-box' style={{backgroundColor: getRandomColor()}}>
+                
+            </div>
         </div>
     </div>
 
