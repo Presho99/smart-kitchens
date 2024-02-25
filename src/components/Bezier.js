b0a0a83eeb7619e9901830f27c591ee557c2ca3f
Precious <Preshomondi@gmail.com>
2024-02-25 23:18:10 +0300

    Adjust curve width

--- src/components/Bezier.js
+++ src/components/Bezier.js
@@ -40,7 +40,7 @@ function Bezier({
     gradient.addColorStop(0, gradientColors[0]);
     gradient.addColorStop(1, gradientColors[1]);
     ctx.strokeStyle = gradient;
-    ctx.lineWidth = 195; // Adjust line width for a thicker curve
+    ctx.lineWidth = 155; // Adjust line width for a thicker curve
     ctx.beginPath();
     ctx.moveTo(p0.x, p0.y);
     ctx.bezierCurveTo(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y);
